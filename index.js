d22ea9e21af78c0c84c5d211141640cf06c38116
Vy Huynh <havy271@gmail.com>
2022-07-25 15:28:41 +0930

    edit packagejson

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ const app = express();
 
 dotenv.config();
 
-app.use(cors({origin: 'http://localhost:3000'}));
+app.use(cors());
 app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: false }));``
+app.use(bodyParser.urlencoded({ extended: false }));
 
 
 // Connect to DB
